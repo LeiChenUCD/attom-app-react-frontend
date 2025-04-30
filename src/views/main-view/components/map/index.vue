f91fc9c1d2c787376c91b9347f65963f77b816f3
yeyuliang2015 <406437752@qq.com>
2025-04-30 17:59:04 +0800

    tes

--- src/views/main-view/components/map/index.vue
+++ src/views/main-view/components/map/index.vue
@@ -691,7 +691,7 @@ function getElemTop() {
     if (filter && elements?.length > 1) {
       elements[1].addEventListener("scroll", function () {
         offsetTop.value = this.scrollTop - filter.offsetHeight;
-        if (window.innerWidth < 760) {
+        if (window.innerWidth <= 760) {
           const overviewBox = document.getElementById("overview-main-box");
           const tableBox = document.getElementById("table-main-box");
            offsetTop.value = offsetTop.value - overviewBox?.offsetHeight - tableBox?.offsetHeight;
