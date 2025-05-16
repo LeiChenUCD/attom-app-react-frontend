dc798b0732a3d98ce4a7f60e556f9d0efd368106
yuliangye3 <406437752@qq.com>
2025-05-17 06:53:59 +0800

    feat: add query page

--- src/views/main-view/components/table/index.vue
+++ src/views/main-view/components/table/index.vue
@@ -23,6 +23,25 @@ const props = defineProps({
       return [];
     }
   },
+  paginationParams: {
+    type: Object,
+    default: () => {
+      return {
+        currentPage: 1,
+        pageSize: 500,
+        pageSizes: [
+          100, 200, 300, 400, 500, 1000, 2000, 3000, 4000, 5000, 10000, 15000,
+          20000, 25000, 30000, 35000, 40000, 45000, 50000, 100000, 1000000
+        ],
+        size: "default",
+        background: false
+      };
+    }
+  },
+  dataTotal: {
+    type: Number,
+    default: () => 0
+  },
   sortState: {
     type: null as PropType<any>
   }
@@ -32,9 +51,11 @@ const parentContainer = ref(null);
 const boxWidth = ref(500);
 const boxHeight = ref(500);
 const tableColumns: any[] = props.columns || [];
-const emit = defineEmits(["onSort", "onRowIndex"]);
+const emit = defineEmits(["onSort", "onRowIndex", "onPage"]);
 const currentRowIndex = ref(0);
 const tableRef = ref();
+const currentPage = ref(props.paginationParams.currentPage || 1);
+const pageSize = ref(props.paginationParams.pageSize || 500);
 
 function onSortTable(sortBy: SortBy) {
   emit("onSort", sortBy);
@@ -43,6 +64,24 @@ function onSortTable(sortBy: SortBy) {
 function onClickRow(index: number) {
   currentRowIndex.value = index;
   emit("onRowIndex", index);
+}
+
+const handleSizeChange = (val: number) => {
+  currentPage.value = 1;
+  pageSize.value = val || 500;
+  onBackPage();
+};
+
+const handleCurrentChange = (val: number) => {
+  currentPage.value = val;
+  onBackPage();
+};
+
+function onBackPage() {
+  emit("onPage", {
+    currentPage: currentPage.value,
+    pageSize: pageSize.value
+  });
 }
 
 onMounted(() => {
@@ -66,6 +105,21 @@ watch(
 
 <template>
   <div ref="parentContainer" v-loading="loading" class="main-view-table">
+    <div style="margin: 10px 0; display: flex; justify-content: end">
+      <el-pagination
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+        :page-sizes="paginationParams.pageSizes"
+        :size="paginationParams.size"
+        :disabled="false"
+        :background="paginationParams.background"
+        layout="sizes, prev, pager, next, jumper"
+        :total="dataTotal"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
+
     <el-table-v2
       :columns="tableColumns"
       :data="houses"
